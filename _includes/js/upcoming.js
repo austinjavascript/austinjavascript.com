bb353be418d5e2888a66b806e7476c5b0e2e23e5
Aaron Stacy <aaron.r.stacy@gmail.com>
2017-07-14 12:46:14 -0500

    fix race in front page code

--- _includes/js/upcoming.js
+++ _includes/js/upcoming.js
@@ -1,10 +1,14 @@
 (function() {
   var script = document.currentScript;
-  var container = script.parentElement.parentElement;
-  var date = container.querySelector('x-date').when();
+  var previousOnload = window.onload || function(){};
+  window.onload = function() {
+    previousOnload();
+    var container = script.parentElement.parentElement;
+    var date = container.querySelector('x-date').when();
 
-  if (date < new Date()) {
-    container.className = 'hidden';
-    document.getElementById('no-upcoming-meetups').className = '';
-  }
+    if (date < new Date()) {
+      container.className = 'hidden';
+      document.getElementById('no-upcoming-meetups').className = '';
+    }
+  };
 })();
