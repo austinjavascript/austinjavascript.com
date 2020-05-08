5651c5a13194973d8624842a64b402da7a6ad7d2
Steve Stedman <steve@stedman.dev>
2020-05-08 10:29:09 -0500

    Add meetups output to posts, set up redirects; resolves #115

--- .eleventy.js
+++ .eleventy.js
@@ -71,6 +71,24 @@ module.exports = (eleventyConfig) => {
     return input;
   });
 
+  // BROWSERSYNBC: add ability to see 404.html in dev mode
+  eleventyConfig.setBrowserSyncConfig({
+    callbacks: {
+      ready(err, bs) {
+        bs.addMiddleware('*', (req, res) => {
+          const content_404 = fs.readFileSync('_site/404.html');
+          // Provides the 404 content without redirect.
+
+          res.write(content_404);
+          // Add 404 http status code in request header.
+          // res.writeHead(404, { "Content-Type": "text/html" });
+          res.writeHead(404);
+          res.end();
+        });
+      },
+    },
+  });
+
   return {
     dir: {
       input: './',
