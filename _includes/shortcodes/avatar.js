1e70e3d9514da5f4b68918e6a1d417689bb30509
Steve Stedman <steve@stedman.dev>
2020-05-20 15:41:58 -0500

    Fix eleventy-img fetch 404 issue

--- _includes/shortcodes/avatar.js
+++ _includes/shortcodes/avatar.js
@@ -1,20 +1,27 @@
 const Image = require('@11ty/eleventy-img');
 
 module.exports = async (src, alt, className, outputFormat = 'jpeg') => {
-  const stats = await Image(src, {
-    cacheDuration: '12w',
-    formats: [outputFormat],
-    outputDir: '_site/assets/avatar/',
-    widths: [96],
-  });
-
-  const classAttr = className ? `class="${className}"` : '';
-
-  const props = stats[outputFormat].pop();
-
   if (alt === undefined) {
     throw new Error(`Missing ALT attribute from: ${src}`);
   }
 
-  return `<img ${classAttr} src="${props.outputPath.replace('_site', '')}" alt="${alt}" loading="lazy">`;
+  const classAttr = className ? `class="${className}"` : '';
+
+  try {
+    const stats = await Image(src, {
+      cacheDuration: '12w',
+      formats: [outputFormat],
+      outputDir: '_site/assets/avatar/',
+      widths: [96],
+    });
+
+    const props = stats[outputFormat].pop();
+
+    return `<img ${classAttr} src="${props.outputPath.replace('_site', '')}" alt="${alt}" loading="lazy">`;
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error(err);
+
+    return '';
+  }
 };
