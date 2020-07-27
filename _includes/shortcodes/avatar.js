639d9b540dd95ffc743cde27238601d30c71893b
Steve Stedman <steve@stedman.dev>
2020-07-27 18:15:20 -0500

    Fix avatar image cache

--- _includes/shortcodes/avatar.js
+++ _includes/shortcodes/avatar.js
@@ -2,14 +2,19 @@ const Image = require('@11ty/eleventy-img');
 
 module.exports = async (src, alt, className, outputFormat = 'jpeg') => {
   if (alt === undefined) {
-    throw new Error(`Missing ALT attribute from: ${src}`);
+    throw new Error(`Avatar: missing ALT attribute from: ${src}`);
   }
 
   const classAttr = className ? `class="${className}"` : '';
 
   try {
     const stats = await Image(src, {
-      cacheDuration: '12w',
+      cacheOptions: {
+        // renew img cache every 12 weeks
+        duration: '12w',
+        // version control the img cache so GitHub Pages doesn't start from scratch
+        directory: '_cache',
+      },
       formats: [outputFormat],
       urlPath: '/assets/avatar/',
       outputDir: '_site/assets/avatar/',
@@ -21,8 +26,9 @@ module.exports = async (src, alt, className, outputFormat = 'jpeg') => {
     return `<img ${classAttr} src="${props.url}" alt="${alt}" loading="lazy">`;
   } catch (err) {
     // eslint-disable-next-line no-console
-    console.error(err);
+    console.error('Avatar: eleventy-img error:', err);
 
-    return '';
+    // load empty img src (as placeholder)
+    return `<img ${classAttr} src="" alt="${alt}" loading="lazy">`;
   }
 };
