4e28b05dc4b5004bf74564dd1e8d94a5e5fa76d4
Steve Stedman <steve@stedman.dev>
2022-11-12 20:08:08 -0600

    Fixed missing CSS by adding purgecss config

---
+++ purgecss.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  css: ['_site/assets/css/main.css'],
+  content: [
+    '_site/index.html',
+    '_site/about/index.html',
+    '_site/code-of-conduct/index.html',
+    '_site/contributing/index.html',
+    '_site/posts/index.html',
+    '_site/posts/meetups/index.html',
+    '_site/posts/meetups/2020/**/*.html',
+  ],
+  output: '_site/assets/css/main.css',
+};
